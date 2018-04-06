67f185d17fba570d960cc774a5bf36876a93c04d
fightingm <417300459@qq.com>
2018-04-07 01:00:39 +0800

    add

--- http与url模块/url模块.js
+++ http与url模块/url模块.js
@@ -2,8 +2,8 @@ const http = require('http');
 const url = require('url');
 
 http.createServer((req, res) => {
+    
     res.writeHead(200, {"Content-Type": "text/html, charset='utf-8"});
-
     if(req.url !== '/favicon.ico') {
         // 第一个参数表示要解析的url，第二个参数表示是否把get传值解析成对象的形式
         const result = url.parse(req.url, true);
