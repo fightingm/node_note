cac83bdf51d1f2b62082b145959102e74a570043
fightingm <417300459@qq.com>
2018-04-06 21:56:34 +0800

    add

--- fs模块/03/fs03.js
+++ fs模块/03/fs03.js
@@ -3,53 +3,55 @@ const fs = require('fs');
 
 // createReadStream 创建读取流 
 // 当读取文件较小时使用readFile
-// const readStream = fs.createReadStream('1.md');
+const readStream = fs.createReadStream('1.md');
 
-// let data = '';
-// let count = 0;
-// // 正在读取
-// readStream.on('data', chunk => {
-//     data += chunk;
-//     count ++;
-// });
+let data = '';
+let count = 0;
+// 正在读取
+readStream.on('data', chunk => {
+    data += chunk;
+    count ++;
+});
 
-// // 读取完成
-// readStream.on('end', () => {
-//     console.log(data);
-//     console.log(count);
-// });
-// // 读取失败
-// readStream.on('error', err => {
-//     console.log(err);
-// });
+// 读取完成
+readStream.on('end', () => {
+    console.log(data);
+    console.log(count);
+});
+// 读取失败
+readStream.on('error', err => {
+    console.log(err);
+});
 
 // createWriteStrm 创建写入流
 // 当写入内容较小时使用writeFile或者appendFile
 
 // 创建一个写入流，写入到2,md
-// const writeStream = fs.createWriteStream('2.md');
+const writeStream = fs.createWriteStream('2.md');
 
-// // 写入
-// writeStream.write('test md', 'utf8');
-// // 停止写入 ，在这之后再调用write方法会报错
-// writeStream.end();
+// 写入
+writeStream.write('test md', 'utf8');
+// 停止写入 ，在这之后再调用write方法会报错
+writeStream.end();
 
-// // 写入完成
-// writeStream.on('finish', () => {
-//     console.log('写入完成');
-// });
-// // 写入失败
-// writeStream.on('error', err => {
-//     console.log(err);
-// });
+// 写入完成
+writeStream.on('finish', () => {
+    console.log('写入完成');
+});
+// 写入失败
+writeStream.on('error', err => {
+    console.log(err);
+});
 
 //pipe 一边读一边写
-// fs.createReadStream('2.md').pipe(fs.createWriteStream('4.md'));
-const readStream = fs.createReadStream('2.md');
-const writeStream = fs.createWriteStream('5.md');
-readStream.on('data', chunk => {
-    writeStream.write(chunk, 'utf8');
-});
-readStream.on('end', () => {
-    writeStream.end();
-})
+fs.createReadStream('2.md').pipe(fs.createWriteStream('4.md'));
+
+// 相当于下面这种方式
+// const readStream = fs.createReadStream('2.md');
+// const writeStream = fs.createWriteStream('5.md');
+// readStream.on('data', chunk => {
+//     writeStream.write(chunk, 'utf8');
+// });
+// readStream.on('end', () => {
+//     writeStream.end();
+// });
